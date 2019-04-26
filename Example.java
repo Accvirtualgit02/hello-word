847be422f1dfafdedd87671d3d16f377181ab888
Accvirtualgit02 <49898659+Accvirtualgit02@users.noreply.github.com>
2019-04-26 15:19:02 +0700

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,18 @@
+/*
+	Day la chuong trinh java don gian
+	File nay co ten la: Example.java
+*/
+class Example {
+	//chuong trinh java bat dau bang loi goi phuong thuc main()driver 
+	public static void main(String args[]) {
+		system.out.println("Hello word!!!");
+}
+
+/*
+*	Name: Bui Van Hieu.
+*	Code of student: 17211TT2092.
+*	Class: 17211TT2092.
+*	Homework chap 5.
+*	Lesson: 
+*	Create of date: 
+*/
